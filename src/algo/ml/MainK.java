ff0e0d235fd6485bf8cdfd1ac46c96b82ba7efb3
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2022-03-20 21:40:44 +0100

    add intraClassInertia totalInertia mean to kmeans

--- src/algo/ml/MainK.java
+++ src/algo/ml/MainK.java
@@ -1,6 +1,6 @@
 package algo.ml;
 
-import java.util.ArrayList;
+
 
 import algo.ml.kmeans.Cluster;
 import algo.ml.kmeans.Kmeans;
@@ -15,16 +15,14 @@ public static void main(String[] args) throws Exception {
 	 double t [] = {1,2,3,4,5};
 	 double s[][] = { 
 				{ 1, 6, 9, 0},
-				{ 26, 23, 16, 19 },
+				{ 26, 23, 16, 19 },	
 				{ 39, 36, 32, 36 },
 				{ 48, 44, 48, 47 },
-				{ 54, 49, 64, 56 } };
-	Kmeans km = new Kmeans(5);
-	ArrayList<Double> test = new ArrayList<Double>();
-	for (int i = 0; i < s.length; i++) {
-		test.add(2.6);
-	}
-	//System.out.println(test);
+				{ 6, 49, 64, 56 } };
+	Kmeans km = new Kmeans(2);
+	System.out.println( km.gravityCenter(s));
+	System.out.println(km.totalInertia(km.gravityCenter(s), s));
+
 	km.KmeansAlgorith(4, s);
 	//System.out.println(km.euclidienneDistance(c, t));
 }
