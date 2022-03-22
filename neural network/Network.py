7f7732f975ca242ccadccfda92487a6277720765
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2022-03-22 10:12:24 +0100

    add sigmoide and tangente function get and etter

--- neural network/Network.py
+++ neural network/Network.py
@@ -1,3 +1,5 @@
+from Function import *
+
 '''
    this file contains full neural network
 '''
@@ -14,10 +16,10 @@ class Network:
         self.name = name
         #by default , many person write hyperbolic tangente by Tangente
         if 'tangente'== str.lower(learn):
-       
+            self.function_learn = tangente
             self.name_function_learn = 'tangente'
         else:
-         
+            self.function_learn = sigmoide
             self.name_function_learn = 'sigmoide'
         
         self.error = error 
@@ -27,5 +29,42 @@ class Network:
 
         self.control = 0  
 
+    def get_name(self):
+        return self.name 
+
+    def set_name(self , name):
+        self.name = name
+
+    def get_error(self):
+        return self.error
+    
+    def set_error(self , error):
+        if errore >0 :
+            self.error = error
+        else:
+            raise ValueError("eror must be positif")
+    
+    def set_function_learn(self , name):
+        if (str.lower(name)=='tangente'):
+            self.function_learn = tangente
+            self.name_function_learn = 'tangente'
+        else:
+            self.function_learn = sigmoide
+            self.name_function_learn = 'sigmoide'
+    
+    def get_name_function_learn(self):
+        return self.name_function_learn
+    
+    def get_number_of_layer(self):
+        return len(self.layer)
+
+    def get_last_layer(self):
+        return self.values[-1]
+
+    def get_data(self): # return NN
+        return [self.get_name() , self.get_name_function_learn() ,self.get_error() ,self.get_number_of_layer()]
 
 
+
+h= Network()
+print(h.get_data())
