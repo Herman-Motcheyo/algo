8d325b89fe78dbb0dacfe960155d23b20dd0af17
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2022-03-22 09:34:08 +0100

    adding constructor

--- neural network/Network.py
+++ neural network/Network.py
@@ -3,3 +3,29 @@
 '''
 
 class Network:
+
+    def __init__(self , name ="Unknow", learn ="sigmoide", error =0.001):
+        '''
+           this method is constructor . it used to initialize Neural Network(NN)
+           name : it is name of NN by default Unknow
+           learn: it is activation function . by default sigmoide
+           error : represente learning error
+        '''
+        self.name = name
+        #by default , many person write hyperbolic tangente by Tangente
+        if 'tangente'== str.lower(learn):
+       
+            self.name_function_learn = 'tangente'
+        else:
+         
+            self.name_function_learn = 'sigmoide'
+        
+        self.error = error 
+        self.layer = [] # array of layer with number of neural by layer
+        self.link = [] #array with all weigth(poid)
+        self.values = [] #array of differents value of NN
+
+        self.control = 0  
+
+
+
